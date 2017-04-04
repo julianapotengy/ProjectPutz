331b1f9129045b7e53934723f89869112c3bc43c
Juliana Potengy <jupotengy@gmail.com>
2017-04-04 13:44:39 -0300

    StartedCutscene
    Player walking, camera moving and apple falling.

---
+++ Assets/Scripts/MainCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+    public GameObject[] borders = new GameObject[2];
+
+    public float max;
+    public float min;
+
+    void Start ()
+    {
+        offset = this.transform.position - player.transform.position;
+        borders = GameObject.FindGameObjectsWithTag("Border");
+        min = borders[0].transform.position.x - 5.5f;
+        max = borders[1].transform.position.x + 5.5f;
+    }
+	
+	void Update ()
+    {
+        //this.transform.position = player.transform.position + offset;
+        if (player.transform.position.x < min && player.transform.position.x > max)
+        {
+            this.transform.position = player.transform.position + offset;
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	private float axisX, axisY, speed;
+    private Rigidbody2D body;
+    private Animator anim;
+    [SerializeField] private GameObject apple;
+
+	void Start ()
+	{
+        speed = 90 * Time.deltaTime;
+        body = this.GetComponent<Rigidbody2D>();
+        anim = this.GetComponent<Animator>();
+	}
+
+    void FixedUpdate()
+    {
+        float move = Input.GetAxis("Horizontal");
+        anim.SetFloat("speed", Mathf.Abs(move));
+    }
+
+    void Update ()
+	{
+        axisX = Input.GetAxis("Horizontal");
+        //axisY = Input.GetAxis("Vertical");
+        body.velocity = new Vector2(axisX * speed, axisY);
+	}
+
+    private void OnTriggerEnter2D(Collider2D p)
+    {
+        if(p.gameObject.name.Equals("Point1"))
+        {
+            apple.GetComponent<Rigidbody2D>().gravityScale = 0.4f;
+        }
+    }
+}
